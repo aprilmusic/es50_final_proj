8fbcd0a37bb67c92392c64ccd4e67e928c2db2c6
aprilmusic <aprilchen01@gmail.com>
2023-04-27 22:54:43 -0400

    connected components

--- image_difference_processing.py
+++ image_difference_processing.py
@@ -1,45 +1,58 @@
 import numpy as np
 from PIL import Image, ImageFilter, ImageOps
 from scipy.spatial import KDTree
-
+from scipy import ndimage
+import matplotlib.pyplot as plt
 
 # Load the two images
-img1 = Image.open('/content/Screenshot 2023-04-21 at 11.29.40 AM.png')
-img2 = Image.open('/content/Screenshot 2023-04-21 at 11.29.50 AM.png')
-
-# Convert the images to RGB format
-rgb1 = img1.convert('RGB')
-rgb2 = img2.convert('RGB')
-
-# Get the dimensions of the images
-width, height = img1.size
+img1 = Image.open('images/science_center1682444448.872606.png')
+img2 = Image.open('images/science_center1682444452.3114848.png')
 
 
-img1 = Image.open('/content/Screenshot 2023-04-21 at 11.29.40 AM.png')
-img2 = Image.open('/content/Screenshot 2023-04-21 at 11.29.50 AM.png')
+def get_diff_locations(img1, img2):
 
-# Convert the images to RGB format
-rgb1 = img1.convert('RGB')
-rgb2 = img2.convert('RGB')
+    # Convert the images to RGB format
+    rgb1 = img1.convert('RGB')
+    rgb2 = img2.convert('RGB')
 
-diff_pixels = np.absolute(np.asarray(rgb1).astype(
-    np.uint16) - np.asarray(rgb2).astype(np.uint16))
-diff_pixels = np.clip(diff_pixels, 0, 256).astype(np.uint8)
-diff_img = Image.fromarray(diff_pixels)
+    # Get the dimensions of the images
+    width, height = img1.size
+    print(width, height)
 
-gray_image = ImageOps.grayscale(diff_img)
+    diff_pixels = np.absolute(np.asarray(rgb1).astype(
+        np.uint16) - np.asarray(rgb2).astype(np.uint16))
+    diff_pixels = np.clip(diff_pixels, 0, 256).astype(np.uint8)
+    diff_img = Image.fromarray(diff_pixels)
 
-# Apply a median filter to remove noise
-filtered_image = gray_image.filter(ImageFilter.MedianFilter(size=3))
+    gray_image = ImageOps.grayscale(diff_img)
 
-# Apply an adaptive threshold to binarize the image
-binary_image = filtered_image.point(lambda x: 255 if x > 65 else 0, mode='1')
+    # Apply a median filter to remove noise
+    filtered_image = gray_image.filter(ImageFilter.MedianFilter(size=3))
 
-# Remove small artifacts using erosion and dilation
-kernel = ImageFilter.Kernel((3, 3), [1, 1, 1, 1, 1, 1, 1, 1, 1])
-eroded_image = binary_image.filter(ImageFilter.MinFilter(size=3))
-dilated_image = eroded_image.filter(ImageFilter.MaxFilter(size=3))
+    # Apply an adaptive threshold to binarize the image
+    binary_image = filtered_image.point(
+        lambda x: 255 if x > 65 else 0, mode='1')
 
-# Save the grayscale image
-dilated_image.save('gray_image.jpg')
-dilated_image.show()
+    # Remove small artifacts using erosion and dilation
+    kernel = ImageFilter.Kernel((3, 3), [1, 1, 1, 1, 1, 1, 1, 1, 1])
+    eroded_image = binary_image.filter(ImageFilter.MinFilter(size=3))
+    dilated_image = eroded_image.filter(ImageFilter.MaxFilter(size=3))
+
+    # Save the grayscale image TODO DELETE THIS LATER
+    dilated_image.save('gray_image.jpg')
+    dilated_image.show()
+
+    labeled, nr_objects = ndimage.label(np.asarray(dilated_image))
+    for i in range(nr_objects):
+        # If they're really small, skip them
+        if np.sum(labeled == i+1) < 1100:
+            continue
+        labeled_1 = np.zeros(labeled.shape) + (labeled == i+1)
+        plt.imshow(labeled_1)
+        plt.show()
+        # TODO CONVERT THESE TO PIXELS TO DISPLAY ON THE BOARD AND RETURN THOSE INSTEAD
+
+    return np.asarray(dilated_image)
+
+
+get_diff_locations(img1, img2)
