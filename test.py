158d56fd0ba7ea4145a3dcbb65e6c5f1c55650b9
aprilmusic <aprilchen01@gmail.com>
2023-05-02 15:52:41 -0400

    april test

--- test.py
+++ test.py
@@ -9,18 +9,25 @@ import serial
 
 import time
 
-arduinoData = serial.Serial('/dev/cu.usbmodem101', 115200)
+arduinoData = serial.Serial('/dev/cu.usbmodem1101', 115200)
 # for i in range(3):
 time.sleep(2)
 
 # arduinoData.write(bytes(
 # '10,10,12,12,14,14,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00', 'utf-8'))
-# arduinoData.write(bytes('10,10,12,12', 'utf-8'))
-time.sleep(1)
+# arduinoData.write(bytes("605040302010", 'utf-8'))
+arduinoData.write(bytes(';10,;12,;14,;16,;18,;20,', 'utf-8'))
 
-arduinoData.write(bytes('15,30', 'utf-8'))
-time.sleep(1)
+# time.sleep(1)
 
+# arduinoData.write(bytes('15,30', 'utf-8'))
+# time.sleep(5)
 
-arduinoData.write(bytes('3,6', 'utf-8'))
-time.sleep(1)
+# arduinoData.write(bytes('new', 'utf-8'))
+# time.sleep(5)
+
+# arduinoData.write(bytes('10,12', 'utf-8'))
+# time.sleep(1)
+
+# arduinoData.write(bytes('3,6', 'utf-8'))
+# time.sleep(1)
